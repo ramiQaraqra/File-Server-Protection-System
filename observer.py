08e8032f90388b6088decbb55d381349fb34b71e
MohamedAli74 <mohamedali243.123@gmail.com>
2025-12-18 20:00:37 +0200

    Refactor file handling to use logging and implement file isolation functionality

--- observer.py
+++ observer.py
@@ -5,7 +5,8 @@ import sys
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from scanner import scan_file
-
+from logger import log
+from utils import isolate_file
 
 class FileSecurityHandler(FileSystemEventHandler):
     processing_files = set()
@@ -19,24 +20,26 @@ class FileSecurityHandler(FileSystemEventHandler):
              self.process(event.src_path, "MODIFIED")
 
     def process(self, path, event_type):
+        time.sleep(1.0)  # Ensure file operations are complete
         if path in self.processing_files:
-            print(f"[{event_type}] Ignored: File {os.path.basename(path)} is already being processed.")
+            log(f"[{event_type}] Ignored: File {os.path.basename(path)} is already being processed.")
             return
 
-        print(f"\n--- FILE {event_type} DETECTED ---")
+        log(f"\n--- FILE {event_type} DETECTED ---")
         self.processing_files.add(path)
         
         try:
-            print(f"Scanning file: {path}")
+            log(f"Scanning file: {path}")
             scan_result = scan_file(path)
-            print(f"Scan result: {scan_result}")
+            log(f"Scan result: {scan_result}")
             
             if scan_result.startswith("Infected"):
-                #isolate the file if it was infected
-                print("THREAT DETECTED!! This file needs to be isolated/logged.")
-            
+                log("THREAT DETECTED!! This file needs to be isolated/logged.")
+                threat_name = scan_result.split("(", 1)[1].rstrip(")")  
+                isolate_file(path, threat_name)
+                            
         except Exception as e:
-            print(f"An error occurred during scan process: {e}")
+            log(f"An error occurred during scan process: {e}")
         
         finally:
             self.processing_files.discard(path)
@@ -47,7 +50,7 @@ if __name__ == "__main__":
         new_path = sys.argv[1].strip()
         with open('paths.txt', 'a') as f:
             f.write(f"\n{new_path}")
-        print(f"Added new path: {new_path}")
+        log(f"Added new path: {new_path}")
 
     event_handler = FileSecurityHandler()
     observer = Observer()
@@ -60,18 +63,18 @@ if __name__ == "__main__":
             if not path:
                 continue
             if not os.path.isdir(path):
-                print(f" [NEW] Creating directory: {path}")
+                log(f" [NEW] Creating directory: {path}")
                 try:
                     os.makedirs(path, exist_ok=True)
                 except OSError as e:
-                    print(f" [ERR] Could not create {path}: {e}")
+                    log(f" [ERR] Could not create {path}: {e}")
                     continue
 
             observer.schedule(event_handler, path, recursive=True)
-            print(f" [OK] Monitoring: {path}")
+            log(f" [OK] Monitoring: {path}")
 
     observer.start()
-    print(f"\nSTARTING MONITORING... Drop or Modify files to test.")
+    log(f"\nSTARTING MONITORING... Drop or Modify files to test.")
     try:
         while True:
             time.sleep(1)
---
+++ utils.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import time
+import subprocess
+from logger import log
+
+#this folder should have permissions set so only Admin can read it.
+ISOLATION_FOLDER = "C:/Server_Quarantine"
+
+def isolate_file(infected_file_path, threat_name):
+    if not os.path.exists(ISOLATION_FOLDER):
+        create_secure_quarantine("C:/Server_Quarantine")
+
+    # We add a timestamp so if multiple uploads occurd at the same time with the same name, we don't overwrite.
+    filename = os.path.basename(infected_file_path)
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    
+    new_name = f"{timestamp}_{filename}.infected"
+    destination = os.path.join(ISOLATION_FOLDER, new_name)
+
+    try:
+        log(f"!!! MOVING INFECTED FILE TO: {destination}")
+        shutil.move(infected_file_path, destination)
+        
+        warning_note = infected_file_path + ".txt"
+        with open(warning_note, "w") as f:
+            f.write(f"The file '{filename}' was removed by Server Protection.\n")
+            f.write(f"Reason: Malware Detected ({threat_name})\n")
+            f.write("Contact the Administrator if you believe this is an error.")
+            
+    except Exception as e:
+        log(f"Error isolating file: {e}")
+
+
+def create_secure_quarantine(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        # Set permissions to allow only Admin access
+    try:
+        subprocess.run(
+            ['icacls', path, '/inheritance:r', '/grant:r', 'Administrators:(OI)(CI)F'], 
+            check=True, 
+            stdout=subprocess.DEVNULL
+        )
+        print(f" [SECURE] Quarantine locked. Only Admins can access: {path}")
+    except Exception as e:
+        print(f" [WARNING] Could not set permissions: {e}")
